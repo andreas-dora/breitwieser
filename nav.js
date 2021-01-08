a0439626ee789bb7d8caaaa85048d08c458085cc
andreas-dora <andreas.dora@icloud.com>
2021-01-08 18:08:34 +0100

    MEnu geölt

--- nav.js
+++ nav.js
@@ -1,10 +1,18 @@
-const overlay = document.getElementById('id_pannel');
+// const overlay = document.getElementById('id_pannel');
+const overlay = document.querySelector('.pannelBox');
 const openBtn = document.getElementById('id_menu-btn');
 openBtn.addEventListener('click', openFun);
 const closeBtn = document.getElementById('id_closeBtn').addEventListener('click', closeFun);
 
+var pLink = document.querySelectorAll('.pannelLink');
+
 function openFun(){
-  overlay.style.height = "100%";
+  overlay.classList.toggle('pannelOpen');
+  if(openBtn.innerHTML === 'Menu'){
+    openBtn.innerHTML = 'close';
+  } else {
+  openBtn.innerHTML = 'Menu';
+  }
 }
 
 function closeFun(){
